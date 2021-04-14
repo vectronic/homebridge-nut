9ea134157cad99d0ffe22e793c06c7efd49a6ccb
vectronic <hello.vectronic@gmail.com>
2021-04-14 12:22:23 +0100

    fix: add default state values

--- index.js
+++ index.js
@@ -314,6 +314,7 @@ NutAccessory.prototype.getServices = function () {
     contactSensorService.addCharacteristic(Characteristic.UpsPowerConsumption);
     contactSensorService.addCharacteristic(Characteristic.UpsPowerConsumptionLevel);
 
+    contactSensorService.setCharacteristic(Characteristic.StatusFault, 0);
     contactSensorService.setCharacteristic(Characteristic.CurrentTemperature, 0);
     contactSensorService.setCharacteristic(Characteristic.UpsPowerConsumption, 0);
     contactSensorService.setCharacteristic(Characteristic.UpsPowerConsumptionLevel, 0);
